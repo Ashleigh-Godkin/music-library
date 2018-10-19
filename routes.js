18fcc4152098a9ab7b991455532f9f4b5b4fe470
Glitch (music-library) <none>
2018-10-19 12:58:11 +0000

    💪😎 Updated with Glitch

---
+++ routes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const dashboard = require('./controllers/dashboard.js');
+const playlist = require('./controllers/playlist.js');
+
+const about = require('./controllers/about.js');
+
+router.get('/', dashboard.index);
+router.get('/dashboard', dashboard.index);
+router.get('/about', about.index);
+router.get('/playlist/:id', playlist.index);
+router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
+router.get('/dashboard/deleteplaylist/:playlistid', dashboard.deletePlaylist);
+router.post('/playlist/:id/addsong', playlist.addSong);
+router.post('/dashboard/addplaylist', dashboard.addPlaylist);
+
+
+module.exports = router;
